9ffa21189fa990dedb9612a8f7067ba0a339fde5
Mrityunjay Kumar <133962893+Mrityunjay8@users.noreply.github.com>
2023-08-26 11:07:55 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("tab-contents");
+
+function opentab(tabname) {
+    for (tablink of tablinks) {
+        tablink.classList.remove("active-link");
+    }
+    for (tabcontent of tabcontents) {
+        tabcontent.classList.remove("active-tab");
+    }
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabname).classList.add("active-tab");
+}
+
+var typed = new Typed('#element', {
+    strings: ['Trader', 'Gamer', 'Coder', 'Engineer.'],
+    typeSpeed: 80,
+  });
